26e9b758fe78e12aab6228de631060d1f1f90d1b
SISTEMAS <SISTEMAS@SISTEMAS-PC>
2016-07-05 15:39:03 -0500

    new Commit

--- src/java/datos/Aplicacion.java
+++ src/java/datos/Aplicacion.java
@@ -23,11 +23,11 @@ public class Aplicacion {
             Class.forName("com.mysql.jdbc.Driver");
             //Nombre del servidor. localhost:3306 es la ruta y el puerto de la conexión MySQL
             //panamahitek_text es el nombre que le dimos a la base de datos
-            String servidor = "jdbc:mysql://104.236.81.248:3306/terpel?noAccessToProcedureBodies=true";
+            String servidor = "jdbc:mysql://104.236.81.248:3306/logycus360?noAccessToProcedureBodies=true";
             //String servidor = "jdbc:mysql://localhost:3306/"+bd;
             //El root es el nombre de usuario por default. No hay contraseña
             //Se inicia la conexión
-            conn = DriverManager.getConnection(servidor, "root", "Domi123jn");
+            conn = DriverManager.getConnection(servidor, "logycus360_platf", "5c026fde033e58a1747437ee8468f4b624dab3ee");
             System.out.println("conexion perfecta");           
         }catch ( SQLException excepcionSql){ //excepcionSql = puede ponerle otro nombre
             System.out.println("error en la conexion a la base de datos"+excepcionSql.getMessage());           
